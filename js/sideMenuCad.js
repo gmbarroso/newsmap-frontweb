3883a5305b6b62d17a05e539d31ef54fd090be4f
Guilherme Barroso <barroso.guilherme@gmail.com>
2017-07-25 01:31:47 +0000

    sidemenu de cadastro funcionando

---
+++ js/sideMenuCad.js
@@ -0,0 +1,9 @@
+function abrirCad() {
+    document.getElementById("sidenavCad").style.width = "275px";
+    document.getElementById("pushMapa").style.marginLeft = "275px";
+}
+
+function fecharCad() {
+    document.getElementById("sidenavCad").style.width = "0";
+    document.getElementById("pushMapa").style.marginLeft= "0";
+}
